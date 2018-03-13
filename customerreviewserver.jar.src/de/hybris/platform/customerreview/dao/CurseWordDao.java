4879e71968d387420cf18db58b987dcae58a7a37
Frank-blog <frankliu67@hotmail.com>
2018-03-12 23:55:24 -0400

    first commit

---
+++ customerreviewserver.jar.src/de/hybris/platform/customerreview/dao/CurseWordDao.java
@@ -0,0 +1,16 @@
+package de.hybris.platform.customerreview.dao;
+
+import java.util.List;
+
+public abstract interface CurseWordDao
+{
+  public abstract List<String> getCurseWords();
+  
+ 
+}
+
+
+/* Location:              /Users/TJL4646/CustomerReview_Assignment/customerreviewserver.jar!/de/hybris/platform/customerreview/dao/CustomerReviewDao.class
+ * Java compiler version: 7 (51.0)
+ * JD-Core Version:       0.7.1
+ */
